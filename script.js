6178ca50b766d60185115987c490731ea62c04c3
HarmeetM <hmadhaan620@yahoo.com>
2023-05-27 16:34:56 -0400

    Initial design + layout coded out, just need to move into doing the logic in Javascript

---
+++ script.js
@@ -0,0 +1,18 @@
+//Array to store the books
+let myLibrary = [
+
+];
+
+//Constructor function
+function Book(author, title, pages, hasRead) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.hasRead = hasRead;
+}
+
+//Adding books to the library
+function addBookToLibrary() {
+
+}
+
